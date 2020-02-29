75cb05d3243d954173286dfdcc6d80a7b241cf39
wellchang <2483808264@qq.com>
2020-03-01 01:01:19 +0800

    1.优化google广告为自适应 2.版本号增加透明度

--- app/src/main/java/viz/vplayer/ui/activity/MainActivity.kt
+++ app/src/main/java/viz/vplayer/ui/activity/MainActivity.kt
@@ -5,14 +5,15 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.get
 import androidx.navigation.ui.NavigationUI
 import bolts.Task
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.*
 import com.huawei.hms.analytics.HiAnalytics
 import com.huawei.hms.hmsscankit.ScanUtil
 import com.huawei.hms.ml.scan.HmsScan
@@ -47,6 +48,7 @@ class MainActivity : BaseActivity(), View.OnClickListener {
     lateinit var mo: MyObserver
     private val navController by lazy { findNavController(R.id.main_content) }
     private var loginUtil: LoginUtil? = null
+    private var adView: AdView?=null
 
     override fun getContentViewId(): Int = R.layout.activity_main
     override fun useEventBus(): Boolean = true
@@ -141,11 +143,78 @@ class MainActivity : BaseActivity(), View.OnClickListener {
             MobileAds.initialize(this) {}
             val adRequest = AdRequest.Builder().build()
             runOnUiThread {
-                adView.loadAd(adRequest)
+                if(adView == null) {
+                    adView = AdView(this)
+                    adView!!.id = R.id.adView
+                    constraintLayout_main.addView(
+                        adView,
+                        constraintLayout_main.indexOfChild(imageButton_close_ads) - 1
+                    )
+                    val cs = ConstraintSet()
+                    cs.clone(constraintLayout_main)
+                    cs.connect(
+                        adView!!.id,
+                        ConstraintSet.START,
+                        R.id.constraintLayout_main,
+                        ConstraintSet.START
+                    )
+                    cs.connect(
+                        adView!!.id,
+                        ConstraintSet.END,
+                        R.id.constraintLayout_main,
+                        ConstraintSet.END
+                    )
+                    cs.connect(
+                        adView!!.id,
+                        ConstraintSet.BOTTOM,
+                        R.id.textView_version,
+                        ConstraintSet.TOP
+                    )
+                    cs.connect(
+                        R.id.imageButton_close_ads,
+                        ConstraintSet.END,
+                        R.id.constraintLayout_main,
+                        ConstraintSet.END
+                    )
+                    cs.connect(
+                        R.id.imageButton_close_ads,
+                        ConstraintSet.BOTTOM,
+                        adView!!.id,
+                        ConstraintSet.TOP
+                    )
+                    cs.applyTo(constraintLayout_main)
+                    adView!!.adSize = adSize
+                    adView!!.adUnitId = "ca-app-pub-3940256099942544/6300978111"
+                    adView!!.adListener = object : AdListener() {
+                        override fun onAdLoaded() {
+                            super.onAdLoaded()
+                            imageButton_close_ads.visibility = View.VISIBLE
+                        }
+                    }
+                    adView!!.loadAd(adRequest)
+                }
             }
         }.continueWithEnd("初始化google ads")
     }
 
+    // Determine the screen width (less decorations) to use for the ad width.
+    // If the ad hasn't been laid out, default to the full screen width.
+    private val adSize: AdSize
+        get() {
+            val display = windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+
+            val density = outMetrics.density
+
+            var adWidthPixels = constraintLayout_main.width.toFloat()
+            if (adWidthPixels == 0f) {
+                adWidthPixels = outMetrics.widthPixels.toFloat()
+            }
+
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
 
     fun checkNet() {
         GlobalScope.launch() {
@@ -230,12 +299,17 @@ class MainActivity : BaseActivity(), View.OnClickListener {
                 }
             }
         }
+        imageButton_close_ads.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         val vid = v!!.id
         when (vid) {
-
+            R.id.imageButton_close_ads -> {
+                adView?.destroy()
+                adView?.visibility = View.GONE
+                imageButton_close_ads.visibility = View.GONE
+            }
         }
     }
 
@@ -317,7 +391,7 @@ class MainActivity : BaseActivity(), View.OnClickListener {
                         )
                         navController.navigate(R.id.webActivity, bundle)
                     }
-                    HmsScan.PURE_TEXT_FORM->{
+                    HmsScan.PURE_TEXT_FORM -> {
                         EventBus.getDefault().postSticky(KWEvent(originalValue))
                     }
                 }
